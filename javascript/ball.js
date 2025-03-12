b9b9ad61399aa78803ba2591f0496dcf4dc7dff9
Po-Jan <jan.poljsak@tscng.org>
2025-03-12 12:37:02 +0100

    Add files via upload

--- javascript/ball.js
+++ javascript/ball.js
@@ -1,35 +1,38 @@
 var canvas = document.getElementById("myCanvas");
-		var ctx = canvas.getContext("2d");
-		var x = canvas.width/2;
-		var y = canvas.height-30;
-		var dx = 2;
-		var dy = -2;
-		var ballColor = "#0095DD"
-		
-		var ballRadius = 10;
+var ctx = canvas.getContext("2d");
+var x = canvas.width / 3.3;
+var y = canvas.height - 30;
+var dx = 5;
+var dy = -5;
+var ballColor = "gray";
 
-		function drawBall() {
-			ctx.beginPath();
-			ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-			ctx.fillStyle = ballColor; //"#0095DD"
-			ctx.fill();
-			ctx.closePath();
-		}
-		
-		
+var ballRadius = 10;
 
-		function draw() {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			drawBall();
-			x += dx;
-			y += dy;
-			
-			if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-				dx = -dx;
-			}
-			if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-				dy = -dy;
-			}
-		}
+function drawBall() {
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+	ctx.fillStyle = ballColor;
+	ctx.fill();
+	ctx.closePath();
+}
 
-		setInterval(draw, 12);
+
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx*0.9;
+	y += dy;
+
+	if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+		dx = -dx;
+	}
+	if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+		dy = -dy;
+	}
+}
+
+setInterval(draw, 12);
+
+
+
---
+++ javascript/bricks.js
@@ -0,0 +1,33 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+
+
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            bricks[c][r].x = 0;
+            bricks[c][r].y = 0;
+            ctx.beginPath();
+            ctx.rect(0, 0, brickWidth, brickHeight);
+            ctx.fillStyle = "black";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
